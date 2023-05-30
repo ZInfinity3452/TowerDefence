32e277e53bf8673fdf85a041ef878499e9d3c202
ZInfinity3452 <133793282+ZInfinity3452@users.noreply.github.com>
2023-05-30 19:06:46 +0200

    30.05.2023
    Grafika: gdzieś zaginął zakręt!

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] GameObject kafelek;
+    [SerializeField] GameObject[] kafelek;
     public float rozmiarKafelka;
     
     // Start is called before the first frame update
@@ -21,17 +21,32 @@ public class LevelManager : MonoBehaviour
 
     private void StworzPoziom()
     {
-        rozmiarKafelka = kafelek.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-        Vector2 LewyKamery =  Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
-        for (int y = 0; y < 5; y++)
+        string[] Mapa = new string[]
         {
-            for (int x = 0; x < 5; x++)
+            "0010300",
+            "2222451",
+            "0100372",
+            "0018",
+
+        };
+        int mapX = Mapa[0].ToCharArray().Length;
+        int mapY = Mapa.Length;
+        rozmiarKafelka = kafelek[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        Vector2 LewyKamery = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        for (int y = 0; y < mapY; y++)
+        {
+            char[] jeszczekafle = Mapa[y].ToCharArray();
+            for (int x = 0; x < mapX; x++)
             {
 
-                Instantiate(kafelek, new Vector2( LewyKamery.x + rozmiarKafelka * x, LewyKamery.y + rozmiarKafelka * y), Quaternion.identity);
 
-
+                Kafelek(jeszczekafle[x], rozmiarKafelka, LewyKamery, x, y);
             }
         }
+    }
+    private void Kafelek(char jakikafel, float rozmiarKafelka, Vector2 LewyKamery, int x, int y)
+    {
+        int number = (int)System.Char.GetNumericValue(jakikafel);
+        Instantiate(kafelek[number], new Vector2(LewyKamery.x + rozmiarKafelka * x, LewyKamery.y + rozmiarKafelka * y), Quaternion.identity);
     }
 }
